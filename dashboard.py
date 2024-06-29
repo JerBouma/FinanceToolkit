12533182daf9668de176b76de51342592b7e23a5
jeroen <jer.bouma@gmail.com>
2024-06-29 16:06:03 +0200

    Add in some additional explanation

--- dashboard.py
+++ dashboard.py
@@ -37,7 +37,14 @@ if st.session_state["invalid_api_key"]:
         of the method of calculation.
 
         This dashboard is powered entirely by the Finance Toolkit and gves access to all relevant financial ratios,
-        indicators and performance measurements.
+        indicators and performance measurements. Key features of the dashboard are:
+
+        - Compare any amount of tickers of any range of period (10+ years for Premium users, 5 years for Free users).
+        - Plot any item from the Financial Statements (Balance Sheet, Income Statement, Cash Flow Statement).
+        - Compare a large selection of financial ratios, technical indicators, performance metrics and risk metrics.
+        - Show quarterly results, growth rates, trailing twelve months (TTM) as well as tables (with download
+        to CSV option)
+
         Once you've entered you're API key you are ready to go!
         """
     )
